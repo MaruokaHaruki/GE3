2d6e37607eb618ad513adf16ed5b0fa52d164bf5
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2024-07-24 15:51:06 +0900

    PSOを完全移行しました

--- base/SpriteManager.h
+++ base/SpriteManager.h
@@ -1,37 +1,63 @@
-#pragma once
-
 ///===============================================================///
 /// 
-///SpriteManagerクラス
+///SpriteManagerクラス.h
 /// 
 ///===============================================================///
 //NOTE:スプライト共通部
+
+#include "DirectXManager.h"
+
+#pragma once
 class SpriteManager {
 public:
+	///-------------------------------------------/// 
+	/// メンバ関数
+	///-------------------------------------------///
+
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(DirectXManager* dxManager);
 
+	/// <summary>
+	/// 共通描画設定
+	/// </summary>
+	void CommonDrawSetup();
 
 private:
 
 	/// <summary>
-	/// 
+	/// ルートシグネチャーの作成
 	/// </summary>
 	void CreateRootSignature();
 
 	/// <summary>
-	/// 
+	/// グラフィックスパイプラインの作成
 	/// </summary>
 	void CreateGraphicsPipeline();
 
+public:
+	///-------------------------------------------///  
+	///ゲッター・セッター
+	///-------------------------------------------///
+	/// ===DirectXManager=== ///
+	//Getter
+	DirectXManager* GetDXManager() const { return dxManager_; }
+
+
 
 private:
+	///-------------------------------------------/// 
+	///メンバ変数
+	///-------------------------------------------///
 
+	/// ===DirectXManager.Pointer=== ///
+	DirectXManager* dxManager_;
 
+	/// ===RootSignature=== ///
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_;
 
-
-
+	/// ===グラフィックスパイプライン=== ///
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_;
 };
 
