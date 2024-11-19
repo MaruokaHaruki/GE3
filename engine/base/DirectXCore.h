5537d93109bd6123aaa76cf10dc74f8a09c07a2f
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2024-11-19 15:54:02 +0900

    1

--- engine/base/DirectXCore.h
+++ engine/base/DirectXCore.h
@@ -30,9 +30,9 @@ using namespace Logger;
 //DXtec
 #include"DirectXTex.h"
 //imgui
-#include "externals/imgui/imgui.h"
-#include"externals/imgui/imgui_impl_dx12.h"
-#include "externals/imgui/imgui_impl_win32.h"
+#include "imgui.h"
+#include"imgui_impl_dx12.h"
+#include "imgui_impl_win32.h"
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 #pragma once
