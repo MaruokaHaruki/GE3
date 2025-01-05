cc633caa53a914c37b09284b9b8668e5d7782c5c
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2025-01-05 23:54:10 +0900

    デバッグシーンの初期化と更新処理の追加
    デバッグシーンに音声、2D、3D、パーティクルの初期化と更新処理を追加しました。
    また、`imgui.ini`のウィンドウサイズを変更しました。
    
    詳細:
    - `imgui.ini`:
      - `[Window][2D Object]`のサイズを`500,264`から`500,100`に変更
    - `DebugScene.cpp`:
      - `Initialize`関数に各種初期化コードを追加
      - `Update`関数に各種更新処理を追加
      - `Object2DDraw`関数に2D描画処理を追加
      - `Object3DDraw`関数に3D描画処理を追加
      - `ParticleDraw`関数にパーティクル描画処理を追加
      - `ImGuiDraw`関数にImGuiのデモウィンドウと2Dオブジェクトのウィンドウ表示を追加
    - `DebugScene.h`:
      - 必要なヘッダーのインクルードを追加
      - `private`セクションに各種メンバ変数を追加
    - `GamePlayScene.cpp`:
      - `Initialize`関数から各種初期化コードを削除
      - `Update`関数から各種更新処理を削除
      - `Object2DDraw`関数から2D描画処理を削除
      - `Object3DDraw`関数から3D描画処理を削除
      - `ParticleDraw`関数からパーティクル描画処理を削除
      - `ImGuiDraw`関数に`Hello, GamePlayScene!`のテキスト表示を追加
    - `GamePlayScene.h`:
      - `private`セクションから各種メンバ変数を削除

--- scene/DebugScene.h
+++ scene/DebugScene.h
@@ -10,6 +10,13 @@
 #include "BaseScene.h"
  //========================================
  // Game
+#include "Camera.h"
+#include "Sprite.h"
+#include "Particle.h"
+#include "ParticleEmitter.h"
+#include "Object3d.h"
+#include "Model.h"
+#include "MAudioG.h"
 
 class DebugScene : public BaseScene {
 	///--------------------------------------------------------------
@@ -48,6 +55,37 @@ public:
 	///--------------------------------------------------------------
 	///							メンバ変数
 private:
+	//========================================
+	// オーディオ
+	MAudioG *audio_;
+	//========================================
+	// スプライト
+	std::unique_ptr<Sprite> sprite_;
+	//========================================
+	// 3dオブジェクト
+	std::unique_ptr<Object3d> object3d_;
+	//========================================
+	// パーティクル
+	std::unique_ptr<Particle> particle_;
+	//エミッター
+	std::unique_ptr<ParticleEmitter> particleEmitter_;
+
+	///--------------------------------------------------------------
+	///						 アプリケーション固有
+	Transform transform{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+
+	//========================================
+	// 2Dオブジェクト用
+	//TransformSprite
+	Transform transformSprite{ {256.0f,256.0f,256.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+	//マテリアル
+	Vector4 materialSprite = {1.0f, 1.0f, 1.0f, 1.0f};
+	//UV座標
+	Transform uvTransformSprite{
+		{1.0f,1.0f,1.0f},
+		{0.0f,0.0f,0.0f},
+		{0.0f,0.0f,0.0f},
+	};
 
 
 };
